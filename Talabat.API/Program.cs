1cf109266e9bfb1f5e5fb19eb1bee38e68a79ee9
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-26 23:09:33 -0700

    Part 09 Migration

--- Talabat.API/Program.cs
+++ Talabat.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Talabat.Repository.Data;
+
 namespace Talabat.API
 {
     public class Program
@@ -13,7 +16,13 @@ namespace Talabat.API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(); 
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             #endregion
 
             var app = builder.Build();
